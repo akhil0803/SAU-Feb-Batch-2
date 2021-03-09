d20deb71066a41f63f6e8d1db3db296db5b807ff
Akhil Maddukuri <akhil.maddukuri@accolitedigital.com>
2021-03-10 00:23:44 +0530

    maven assignment added

---
+++ Maven-Evening/source/com/assignment/target/classes/MyServlet.java
@@ -0,0 +1,32 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import java.io.*;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public MyServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.print("Tomcat started");
+	}
+
+}
